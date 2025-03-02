8f73abe353b64fb3e3f98baa87a30cead74acd5a
Mrid22 <mridulaga@outlook.com>
2025-03-02 23:37:58 +0800

    Added clipboard functionality, TODO: make it selection-based

--- src/template-view.tsx
+++ src/template-view.tsx
@@ -1,14 +1,37 @@
 import { ReactElement } from "react";
-import { List} from "@project-gauntlet/api/components";
-import { Environment, Clipboard, showHud } from "@project-gauntlet/api/helpers";
+import { List, Action, ActionPanel} from "@project-gauntlet/api/components";
+import {
+  GeneratedEntrypoint,
+  GeneratedEntrypointAccessory,
+  GeneratedEntrypointAction,
+  Clipboard,
+  showHud
+} from "@project-gauntlet/api/helpers";
 import convert from 'color-convert';
-
 export default function TemplateView(): ReactElement {
   return (
-    <List>
+    <List
+      actions = {
+        <ActionPanel>
+           <Action
+             label={`Copy ${convert.rgb.hsl(255,255,255).toString()} to clipboard`}
+             onAction={ async(id: string | undefined) => {
+               console.log(convert.rgb.hsl(255,255,255).toString());
+               await Clipboard.writeText(convert.rgb.hsl(255,255,255).toString());
+               showHud(`${convert.rgb.hsl(255,255,255).toString()} copied to clipboard`);
+}}
+            />
+            </ActionPanel>
+      }
+    >
       <List.SearchBar/>
-      <List.Item subtitle = {"hsl"} title = {"convert.rgb.hsl(255,255,255)"} id = {"hsl"}></List.Item>
+      <List.Item
+        subtitle = {"hsl"}
+        title = {convert.rgb.hsl(255,255,255).toString()}
+        id = {"hsl"}
+
+        ></List.Item>
       <List.Item subtitle = {"name"} title = {convert.rgb.keyword(255,255,255).toString()} id = {"name"}></List.Item>
     </List>
-    );
+ );
 }

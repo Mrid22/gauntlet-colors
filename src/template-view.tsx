9bc51e105b97c6d287d3f6fdf0ab2c38a2b89272
Mrid22 <153362027+Mrid22@users.noreply.github.com>
2025-03-03 07:16:01 +0000

    Implemented entry specific copy (tbt)

--- src/template-view.tsx
+++ src/template-view.tsx
@@ -14,11 +14,16 @@ export default function TemplateView(): ReactElement {
       actions = {
         <ActionPanel>
            <Action
-             label={`Copy ${convert.rgb.hsl(255,255,255).toString()} to clipboard`}
+             label={`Copy color to clipboard`}
              onAction={ async(id: string | undefined) => {
-               console.log(convert.rgb.hsl(255,255,255).toString());
-               await Clipboard.writeText(convert.rgb.hsl(255,255,255).toString());
-               showHud(`${convert.rgb.hsl(255,255,255).toString()} copied to clipboard`);
+               console.log(id);
+               if (typeof id === "string"){
+                await Clipboard.writeText(id);
+                showHud(`${id} copied to clipboard`);
+               }
+               else{
+                showHud(`please select text to copy`);
+               }
 }}
             />
             </ActionPanel>
@@ -28,10 +33,10 @@ export default function TemplateView(): ReactElement {
       <List.Item
         subtitle = {"hsl"}
         title = {convert.rgb.hsl(255,255,255).toString()}
-        id = {"hsl"}
+        id = {convert.rgb.hsl(255,255,255).toString()}
 
         ></List.Item>
-      <List.Item subtitle = {"name"} title = {convert.rgb.keyword(255,255,255).toString()} id = {"name"}></List.Item>
+      <List.Item subtitle = {"name"} title = {convert.rgb.keyword(255,255,255).toString()} id = {convert.rgb.keyword(255,255,255).toString()}></List.Item>
     </List>
  );
 }
